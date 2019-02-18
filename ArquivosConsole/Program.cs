f1d89253a01b17bafc10e4c9c81cf71e30afd972
Regis Lima <regislima1391@gmail.com>
2019-02-18 14:31:37 -0300

    Comecando solucao

---
+++ ArquivosConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArquivosConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
